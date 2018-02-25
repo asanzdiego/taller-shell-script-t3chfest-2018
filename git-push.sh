9a811ff553d35e80ce946aca26c82af58bd692b8
Adolfo Sanz De Diego <asanzdiego@gmail.com>
2018-02-25 23:03:10 +0100

    actualizado git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -1,18 +1,21 @@
 #! /bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 echo "**************************"
 echo "* PUSH CHANGES TO GITHUB *"
 echo "**************************"
 
-git diff | grep +++
+git status
 
-read -p "You want to continue? [y|*N*]: " OPTION
+read -p "Do you want to continue? [y|*N*]: " OPTION
 
 if [ "$OPTION" == "y" ]; then
 
     read -p "Write the commit message: " MESSAGE
 
-    git add . && \
+    git add -A
     git commit -m "$MESSAGE" && \
-    git push
+    git push origin master
 fi
